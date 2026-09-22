d913412fda6536ef36a23797ad65bcbda2a36e3d
Kusumakar Rokkam <krokkam@student.gitam.edu>
2026-09-23 00:58:03 +0530

    chore(frontend): remove Aether visual layer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,17 +4,12 @@ import {BrowserRouter} from 'react-router-dom';
 import {DraftProvider} from './hooks/ExperimentDraft';
 import App from './App';
 import './styles.css';
-import './styles/reference-product-composition.css';
-import './styles/aether-visual-system.css';
-import {AetherVisualLayer} from './components/AetherVisualLayer';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <DraftProvider>
-        <AetherVisualLayer>
-          <App />
-        </AetherVisualLayer>
+        <App />
       </DraftProvider>
     </BrowserRouter>
   </React.StrictMode>,
